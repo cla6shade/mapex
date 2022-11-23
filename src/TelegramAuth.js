f746f606a29eaaaf2b4ecfbd973988a2484a356a
cla6shade <cla6shade@gmail.com>
2022-11-23 23:05:31 +0900

    404에러가 뜨는 문제 해결을 위해 socks5-http-client 사용

--- src/TelegramAuth.js
+++ src/TelegramAuth.js
@@ -1,11 +1,24 @@
 const TelegramBot = require("node-telegram-bot-api")
+const Agent = require('socks5-https-client/lib/Agent.js')
 
 class TelegramAuth {
     constructor(token) {
         this.token = token;
     }
     login(){
-        return new TelegramBot(this.token, {polling: true});
+        return new TelegramBot(this.token, {
+            polling: true,
+            request: {
+                agentClass: Agent,
+                agentOptions: {
+                    socksHost: process.env.PROXY_SOCKS5_HOST,
+                    socksPort: parseInt(process.env.PROXY_SOCKS5_PORT),
+                    // If authorization is needed:
+                    // socksUsername: process.env.PROXY_SOCKS5_USERNAME,
+                    // socksPassword: process.env.PROXY_SOCKS5_PASSWORD
+                }
+            } // https://stackoverflow.com/questions/54349234/error-polling-error-codeetelegram-messageetelegram-401-unauthorize
+        });
     }
 }
 module.exports = TelegramAuth;
