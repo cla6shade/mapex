a633dc20813e6fb0b46b92c80361edf493d8dcc3
cla6shade <cla6shade@gmail.com>
2022-11-23 23:20:27 +0900

    Revert "/command@username 형태의 메시지 핸들링 하도록 변경, 맵 정보나 조합 정보 조회 시 명령어 메시지에 답장 형태로 전송하도록 변경."
    This reverts commit 41ff7603fd9e9f7641a06c80a1c2387f1f652393.

--- src/TelegramHandler.js
+++ src/TelegramHandler.js
@@ -2,14 +2,6 @@ class TelegramHandler {
     constructor(client, apexApiHandler) {
         this.client = client
         this.apexApiHandler = apexApiHandler
-
-        this.username = ""
-        this.tag = ""
-        this.client.getMe().then((me)=>{
-            this.username = me.username;
-            console.log(this.username + "(으)로 로그인 되었습니다.")
-            this.tag = "@" + this.username;
-        })
     }
 
     bindEventListener() {
@@ -19,15 +11,15 @@ class TelegramHandler {
             let text = message.text;
             let args = text.split(" ");
             let cmd = args.shift();
-            cmd = cmd.toLowerCase()
-            if (cmd === "/map" || cmd === "/map" + this.tag) {
+            if (cmd === "/map") {
                 let timeLeft = this.apexApiHandler.getRotationTimeLeft();
                 let info = this.apexApiHandler.getMapInfo();
                 let current = info.current
                 let map = current.map
                 let next = info.next.map
-                this.client.sendMessage(message.chat.id, "현재 맵은 " + map + " 입니다.(" + timeLeft + " 남음) 다음 맵은 " + next + " 입니다.", {reply_to_message_id: message.message_id});
-            } else if (cmd === "/craft" || cmd === "/craft" + this.tag) {
+                this.client.sendMessage(message.chat.id, "현재 맵은 " + map + " 입니다.(" + timeLeft + " 남음) 다음 맵은 " + next + " 입니다.");
+            } else if (cmd === "/craft") {
+
                 let items = this.apexApiHandler.getCraftItems()
                 if (items.length === 0) {
                     this.client.sendMessage(message.chat.id, "서버에서 정보를 불러오는 중입니다. 잠시만 기다려주세요.")
@@ -38,7 +30,7 @@ class TelegramHandler {
                     let item = items[i]
                     text += item + " ";
                 }
-                this.client.sendMessage(message.chat.id, "현재 제작 로테이션: " + text, {reply_to_message_id: message.message_id})
+                this.client.sendMessage(message.chat.id, "현재 제작 로테이션: " + text)
             }
         });
 
