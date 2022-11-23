d1e622c25ae4c5091a16a2b70eccc55f3fdc4520
cla6shade <cla6shade@gmail.com>
2022-11-23 20:08:43 +0900

    봇 api의 예제대로 이벤트 핸들링 할때 regex로 핸들링 하려 했으나, 안 되는 것 같음. 기존 방식으로 변경

--- src/TelegramHandler.js
+++ src/TelegramHandler.js
@@ -5,28 +5,35 @@ class TelegramHandler {
     }
 
     bindEventListener() {
-        this.client.on("message", console.log);
-        this.client.on(RegExp(/\/map (.+)/), (msg, match) => {
-            let timeLeft = this.apexApiHandler.getRotationTimeLeft();
-            let info = this.apexApiHandler.getMapInfo();
-            let current = info.current
-            let map = current.map
-            let next = info.next.map
-            this.client.sendMessage(msg.chat.id, "현재 맵은 " + map + " 입니다.(" + timeLeft + " 남음) 다음 맵은 " + next + " 입니다.")
-        })
-        this.client.on(RegExp(/\/craft (.+)/), (msg, match) => {
-            let items = this.apexApiHandler.getCraftItems()
-            if (items.length === 0) {
-                this.client.sendMessage(msg.chat.id,"서버에서 정보를 불러오는 중입니다. 잠시만 기다려주세요.")
-                return
+        this.client.on("message", (message) => {
+            if (!message.text)
+                return;
+            let text = message.text;
+            let args = text.split(" ");
+            let cmd = args.shift();
+            if (cmd === "/map") {
+                let timeLeft = this.apexApiHandler.getRotationTimeLeft();
+                let info = this.apexApiHandler.getMapInfo();
+                let current = info.current
+                let map = current.map
+                let next = info.next.map
+                this.client.sendMessage(message.chat.id, "현재 맵은 " + map + " 입니다.(" + timeLeft + " 남음) 다음 맵은 " + next + " 입니다.");
+            } else if (cmd === "/craft") {
+
+                let items = this.apexApiHandler.getCraftItems()
+                if (items.length === 0) {
+                    this.client.sendMessage(message.chat.id, "서버에서 정보를 불러오는 중입니다. 잠시만 기다려주세요.")
+                    return
+                }
+                let text = "";
+                for (let i in items) {
+                    let item = items[i]
+                    text += item + " ";
+                }
+                this.client.sendMessage(message.chat.id, "현재 제작 로테이션: " + text)
             }
-            let text = "";
-            for (let i in items) {
-                let item = items[i]
-                text += item + " ";
-            }
-            this.client.sendMessage(msg.chat.id,"현재 제작 로테이션: " + text)
-        })
+        });
+
     }
 }
 
